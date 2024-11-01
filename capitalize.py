83ecca1499eebd51e0420f36125915e1b9e80d5f
희주 <1two13j@gmail.com>
2024-11-01 12:43:51 +0900

    refactor: capitalize 코드 수정
    - 파일명 수정

---
+++ capitalize.py
@@ -0,0 +1,14 @@
+# 문자열 첫 글자 대문자로
+print('hello'.title()) # Hello
+# 모든 문자열 대문자 변환
+print('foobar'.upper()) # FOOBAR
+# 모든 문자열 소문자 변환
+print('foobar'.lower()) # foobar
+
+# 1자리 미만
+print('foobar'[:1]) # f
+# 1자리 이상
+print('foobar'[1:]) # oobar
+
+# 문자열 교체
+print('h e l l o'.replace(" ", "")) # hello
